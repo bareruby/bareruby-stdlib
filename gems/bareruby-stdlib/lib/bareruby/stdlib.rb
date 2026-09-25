4c5d957c9854552530cb12d323ea126080ad6ba3
uproad <7349115+uproad@users.noreply.github.com>
2026-09-25 19:35:10 +0900

    execute bundle gem command
    bundle gem bareruby-stdlib --no-exe --no-coc --changelog --no-ext --git --github-username=uproad --mit --test=rspec --ci=github --linter=rubocop --no-bundle

---
+++ gems/bareruby-stdlib/lib/bareruby/stdlib.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "stdlib/version"
+
+module Bareruby
+  module Stdlib
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
